9f26c22df08dcb11e2b3255c780a56fd3af2b384
korachiu <kora.chiu@gmail.com>
2025-04-30 12:13:07 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("HELLO THERE! My name is Kora <3");
+console.log("GitHub: https://github.com/korachiu");
+console.log("Instagram: https://www.instagram.com/kora._art/");
+console.log("LinkedIn: www.linkedin.com/in/korachiu-graphic-designer");
+
+<script>
+  const toggleButton = document.getElementById('theme-toggle');
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+</script>
