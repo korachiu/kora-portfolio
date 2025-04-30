6a3ed39d5e6f70e6c6a64c7f02c2fc5a5d6dec4f
korachiu <kora.chiu@gmail.com>
2025-04-30 13:15:20 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+console.log("HELLO THERE! My name is Kora <3");
+console.log("GitHub: https://github.com/korachiu");
+console.log("Instagram: https://www.instagram.com/kora._art/");
+console.log("LinkedIn: www.linkedin.com/in/korachiu-graphic-designer");
